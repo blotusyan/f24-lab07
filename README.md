9c3119bf8c51d46d511edfb7f89948efe3f718bb
Weiheng <weihengyans@gmail.com>
2024-10-11 11:37:57 -0400

    add mock to sendPromoEmail

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Run the following commands to get started:
 mvn install
 mvn test
 ```
+
+* Mock no need to define own class, use mockito
+* stub & fake need wrapper class (method, class interaction)
--- src/test/java/AndrewWebServices/AndrewWebServicesTest.java
+++ src/test/java/AndrewWebServices/AndrewWebServicesTest.java
@@ -2,6 +2,7 @@ package AndrewWebServices;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,7 +20,8 @@ public class AndrewWebServicesTest {
         database = new InMemoryDatabase();
         // recommender = new RecSys();
         recommender = new InMemoryRecSys();
-        promoService = new PromoService();
+        // promoService = new PromoService();
+        promoService = mock(PromoService.class);
 
         andrewWebService = new AndrewWebServices(database, recommender, promoService);
     }
@@ -40,11 +42,53 @@ public class AndrewWebServicesTest {
     public void testSendEmail() {
         // How should we test sendEmail() when it doesn't have a return value?
         // Hint: is there something from Mockito that seems useful here?
+        String testEmail = "test@example.com";
+        // Call the method under test
+        andrewWebService.sendPromoEmail(testEmail);
+
+        // Verify that promoService.mailTo() was called with testEmail
+        verify(promoService).mailTo(testEmail);
     }
 
     @Test
     public void testNoSendEmail() {
         // How should we test that no email has been sent in certain situations (like right after logging in)?
         // Hint: is there something from Mockito that seems useful here?
+        andrewWebService.logIn("Scotty", 17214);
+
+        // Verify that promoService.mailTo() was not called
+        verifyNoInteractions(promoService);
+    }
+
+    @Test
+    public void testMultipleEmails() {
+        String email1 = "user1@example.com";
+        String email2 = "user2@example.com";
+
+        andrewWebService.sendPromoEmail(email1);
+        andrewWebService.sendPromoEmail(email2);
+
+        // Verify that mailTo was called twice with the correct emails
+        verify(promoService).mailTo(email1);
+        verify(promoService).mailTo(email2);
+        verify(promoService, times(2)).mailTo(anyString());
+    }
+    @Test
+    public void testSendEmailException() {
+        String testEmail = "test@example.com";
+
+        // Configure the mock to throw an exception when mailTo is called
+        doThrow(new RuntimeException("Email service down")).when(promoService).mailTo(testEmail);
+
+        try {
+            andrewWebService.sendPromoEmail(testEmail);
+            // fail("Expected RuntimeException to be thrown");
+        } catch (RuntimeException e) {
+            // Assert that the exception message is as expected
+            assertEquals("Email service down", e.getMessage());
+        }
+
+        // Verify that mailTo was called despite the exception
+        verify(promoService).mailTo(testEmail);
     }
 }
